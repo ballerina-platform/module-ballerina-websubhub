3fafc6c27f0033eb6f2b99a9989b9591ab96ed1c
ayeshLK <ayesh9303@gmail.com>
2021-03-16 11:43:14 +0530

    Update java-introp function to use the reactive approach for remote-method-invocation

--- websubhub-native/src/main/java/io/ballerina/stdlib/websubhub/HubNativeOperationHandler.java
+++ websubhub-native/src/main/java/io/ballerina/stdlib/websubhub/HubNativeOperationHandler.java
@@ -19,18 +19,23 @@
 package io.ballerina.stdlib.websubhub;
 
 import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.Future;
 import io.ballerina.runtime.api.Module;
+import io.ballerina.runtime.api.async.Callback;
 import io.ballerina.runtime.api.async.StrandMetadata;
+import io.ballerina.runtime.api.creators.ErrorCreator;
 import io.ballerina.runtime.api.creators.ValueCreator;
 import io.ballerina.runtime.api.types.MethodType;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.api.values.BString;
 
 import java.util.ArrayList;
-import java.util.concurrent.CountDownLatch;
+
+import static io.ballerina.runtime.api.utils.StringUtils.fromString;
 
 /**
  * {@code HubNativeOprationHandler} handles the native method invocations.
@@ -107,20 +112,25 @@ public class HubNativeOperationHandler {
 
     private static Object invokeRemoteFunction(Environment env, BObject bHubService, Object[] args,
                                 String parentFunctionName, String remoteFunctionName) {
+        Future balFuture = env.markAsync();
         Module module = ModuleUtils.getModule();
         StrandMetadata metadata = new StrandMetadata(module.getOrg(), module.getName(), module.getVersion(),
                 parentFunctionName);
-        CountDownLatch latch = new CountDownLatch(1);
-        CallableUnitCallback callback = new CallableUnitCallback(latch);
+        env.getRuntime().invokeMethodAsync(bHubService, remoteFunctionName, null, metadata, new Callback() {
+            @Override
+            public void notifySuccess(Object result) {
+                balFuture.complete(result);
+            }
 
-        env.getRuntime().invokeMethodAsync(bHubService, remoteFunctionName, null, metadata, callback, args);
-
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            // Ignore
-        }
-        return callback.getResult();
+            @Override
+            public void notifyFailure(BError bError) {
+                BString errorMessage = fromString("service method invocation failed: " + bError.getErrorMessage());
+                BError invocationError = ErrorCreator.createError(module, "ServiceExecutionError", 
+                                                    errorMessage, bError, null);
+                balFuture.complete(invocationError);
+            }
+        }, args);
+        return null;
     }
 
 }
